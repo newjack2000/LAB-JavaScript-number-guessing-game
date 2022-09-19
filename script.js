575be20954eaf7a28a530f959f59f531129ca78d
Ronald Baker <83313552+newjack2000@users.noreply.github.com>
2022-09-19 00:00:21 +0000

    added replit files from another program

--- script.js
+++ script.js
@@ -1,3 +1,72 @@
+
+
+
+/*  testing above code.
+
+
+function playGuessingGame(x, y) {
+  //  let numToGuess = randomNum = Math.random() * 11 | 0;
+//    let totalGuesses = 10;
+    //  let guesses = 0;
+    
+    // function playGuessingGame(numToGuess, totalGuesses = 10){
+    
+      let promptText = "Enter a number between 1 and 100.";
+    
+      //for loop. Checks to see if the user doesn't enter anything
+      //or if the user enters a number that is not a number
+    
+    for (guesses = 1; guesses < totalGuesses; guesses++) {
+    
+    //  for (guesses = 1; guesses < totalGuesses) {
+        // removed counter for guesses and added to each if statement
+        //check if the user doesn't enter anything.  If they do not enter anything, exist
+        let num = prompt(promptText);
+    
+        if (num === null) {
+          guesses = 0;
+          return 0;
+        }
+    
+        // checks to see if the value they entered is a number
+        else if (num === "" || isNaN(num)) {
+          promptText = "Please enter a number";
+          // do not need if I remove the counter from the for statement
+          //guesses--;
+        } else if (num > numToGuess) {
+          //guesses++;
+          promptText = "Please enter a number";
+          userGuess = prompt(num + " is too large. Guess a smaller number.");
+        } else if (num < numToGuess) {
+          //guesses++;
+          promptText = "Please enter a number";
+          userGuess = prompt(num + " is too small. Guess a smaller number.");
+        }  else
+          guesses = 0;
+          promptText = "You failed to guess the number.";
+    
+      return guesses;
+      //}
+    
+        var randomNum = Math.random() * 11 | 0,
+    
+    
+      //console.log("The random number is: " + randomNum);
+     // totalGuesses = prompt("How many guesses would you like to make?")
+    
+      // playGuessingGame(randomNum, totalGuesses)
+    
+      let countAttempts = 0;
+      countAttempts = playGuessingGame(7, 8);
+    
+      console.log("You took " + countAttempts + " attempts to guess the number.");
+    
+
+*/
+
+
+/*  intial code
+
 //function playGuessingGame() {
 	// generates a wghole number up to the max 100
 // creating a function to check whether the userinput and the variable is same on the click of a button.
@@ -31,7 +100,7 @@
          console.log("Congatadulations! " + x + " is the correct number!  You guessed the correct number in " + guessCount +" attempts.");
       }
    }
-
+End of initial code  */
   /*
 document.getElementById("submit").onclick=()=>{
 
